da4b46bd9690b22fa7e2ade4fa1042cb86ee47b9
Ran Ben Aharon <ran@ranbena.com>
2011-09-09 22:58:02 +0300

    Refactored and added comments

--- TouchyPHP.php
+++ TouchyPHP.php
@@ -12,23 +12,44 @@ class TouchyPHP {
 
     
     /**
-     * Returns the contents of the CSS/JS file
+     * Returns the contents of the css/js/image file
+     * @param <string> $filename
+     * @param <boolean> $forceInclude {optional}
+     * If set to true or not set, the function returns manipulated content.
+     * If set to false, the function returns original $filename (inside appropriate HTML tag)
+     * @return <string>
      */
     public function getFile($filename, $forceInclude = null){
         $value = $filename;
+        
+        // get file extension
         $format = pathinfo($filename, PATHINFO_EXTENSION);
-    
+        
         if ($forceInclude !== false){
-            $content = file_get_contents($filename);
             switch ($format){
                 case 'css':
-                    $content = self::_replaceImageUrls($content);
-                case 'css':
-                case 'js':
+                    // get file content
+                    $content = file_get_contents($filename);
+                    
+                    // update path of relative image urls
+                    //$content = self::_replaceImageUrls($filename, $content);
+                    
+                    // set content within <style> tag
+                    $value = self::_getInternalTag($format, $content);
+                case 'js':                    
+                    // get file content
+                    $content = file_get_contents($filename);
+                    
+                    // set content within <script> tag
                     $value = self::_getInternalTag($format, $content);
                 default:
-                    if (self::_shouldConvertToBase64($filename)){
-                        $value = self::_getEncodedImage($filename);
+                    // if filename can be converted to image data URI
+                    if (self::_canConvertToImageDataURI($filename)){
+                        // get file content
+                        $content = file_get_contents($filename);
+                        
+                        // set base64 data
+                        $value = self::_getImageDataURI($format, $content);
                     }
             }
         }
@@ -36,54 +57,70 @@ class TouchyPHP {
             switch ($format){
                 case 'css':
                 case 'js':
+                    // set content within <script>/<link> tag
                     $value = self::_getExternalTag($format, $filename);
+                    break;
             }
         }
         return $value;
     }
     
     /**
-     * Gets an image filename and returns if it should be converted to base64
-     * @param <string> $fileName
+     * Gets an image filename and returns if it can be converted to base64
+     * @param <string> $filename
      * @return <boolean>
      */
-    private function _shouldConvertToBase64($filename) {
-        // List of image formats allowed for base64 encoding
+    private function _canConvertToImageDataURI($filename) {
+        // list of image formats allowed for base64 encoding
         $formatArray = array('jpeg', 'jpg', 'jpe', 'png', 'gif');
 
         // Get file format
         $format = pathinfo($filename, PATHINFO_EXTENSION);
+        
         // Remove trailing querystring
         if (strrpos($format, '?')){
             $format = substr($format, 0, strrpos($format, '?'));
         }
 
-        // If the image is not in the list of allowed formats or compression is false, don't encode it, just display it as is
+        // Return whether the file format is allowed for base64 encoding
         return (in_array($format, $formatArray));
     }
     
     /**
      * Gets an image filename and returns it converted to base64 data string
+     * @param <string> $format
+     * @param <string> $content
+     * @return <string> image data URI
      */
-    private static function _getEncodedImage($filename) {
-        // Get file format
-        $format = pathinfo($filename, PATHINFO_EXTENSION);
-        
-        // Do the magic
-        $data = base64_encode(file_get_contents($filename));
+    private static function _getImageDataURI($format, $content) {        
+        // encode
+        $data = base64_encode($content);
 
-        // Return the image URI
+        // return the image URI
         return 'data:image/'.$format.';base64,'.$data;
     }
     
-    private static function _replaceImageUrls($content){
-        return preg_replace_callback('/url\([\'"]?([^\)\'"]+)/',
+    /**
+     * Searches for url(filepath) in content and returns manipulated according to php page location
+     * @param <string> $format
+     * @param <string> $content
+     * @ignore
+     */
+    private static function _replaceImageUrls($filename, $content){
+        $content = preg_replace_callback('/url\([\'"]?([^\)\'"]+)/',
                                         function($m){
-                                            return str_replace($m[1], '*** REPLACE THIS RANBENA ***', $m[0]);
+                                            return str_replace($m[1], '*** REPLACE string ***', $m[0]);
                                         },
                                         $content);
+        return $content;
     }
     
+    /**
+     * Returns HTML tag according to format, pointing to an external resource
+     * @param <string> $format
+     * @param <string> $filename
+     * @return <string> HTML tag
+     */
     private static function _getExternalTag($format, $filename){
         switch ($format) {
             case 'css':
@@ -96,6 +133,12 @@ class TouchyPHP {
         return str_replace('{$filename}', $filename, $template);
     }
     
+    /**
+     * Returns HTML tag according to format, including content
+     * @param <string> $format
+     * @param <string> $content
+     * @return <string> HTML tag
+     */
     private static function _getInternalTag($format, $content){
         switch ($format){
             case 'css':
